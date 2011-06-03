0d67f8fd4d9224523806201b721cf95e8d169626
Naresh Kapse <kapse.naresh@gmail.com>
2011-06-03 18:18:50 +0530

    shallow commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Studyp::Application.config.secret_token = 'be42f301602686dc9ef19383c773c6cfde90629a8a70a02368590152e037dede014386454edb929ff77840d965ec1cefb0da52a0c17ec6a12ab9702d46eacaaf'
