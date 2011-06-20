b16b1e1a2b17101a91fd6a36282ee1cc2100fbd6
Naresh Kapse <kapse.naresh@gmail.com>
2011-06-20 07:50:16 +0530

    added university filter for search

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,14 +10,34 @@ class HomeController < ApplicationController
   end
   
   def eamcet_results
-    if request.get?
+    if request.get? and params.empty?
       render :action => :eamcet_index
       return
     end
     
     @data=params[:eamcet] || params
+    @data[:affl] = @data[:affl].delete_if{|x| x.empty?}
+    params=@data
+    filters=get_conditions
+    @results = CutoffScore.paginate :select => "colleges.id as college_id ", :conditions => filters, :include => [:college, :course], :page => params[:page]
+  end
+  
+  private
+  def get_conditions
     row="#{@data[:caste]}-#{@data[:gender]}".underscore
     rank=@data[:rank]
-    @results = CutoffScore.paginate :select => "colleges.id as college_id ", :conditions => ["#{row} >= ?",rank], :include => [:college, :course], :page => params[:page]
+    
+    filter_cond=[]
+    filters="#{row} >= ?"
+    
+    affls=@data[:affl]
+    affls=affls.collect{|x| x if x!=""}.compact
+    
+    filters = filters + " and affl in (?)" unless affls.empty?
+
+    filter_cond << filters
+    filter_cond << rank.to_i
+    filter_cond << affls unless affls.empty?
+    filter_cond
   end
 end
